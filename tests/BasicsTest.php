ae33efe92543f216a865a615f8a7c33ae05f89e7
Vitaliy Parkhomenko <radvers@gmail.com>
2020-07-06 19:22:12 +0300

    homework: converting types

--- tests/BasicsTest.php
+++ tests/BasicsTest.php
@@ -16,13 +16,13 @@ class BasicsTest extends TestCase
         $this->assertEquals('testMagicConstants', __FUNCTION__);
 
         // The function name, or {closure} for anonymous functions.
-        // TODO __CLASS__
+        $this->assertEquals('BasicsTest', __CLASS__);
 
         // The class method name.
-        // TODO __METHOD__
+        $this->assertEquals('BasicsTest::testMagicConstants', __METHOD__);
 
         // The name of the current namespace.
-        // TODO __NAMESPACE__
+        $this->assertEquals('', __NAMESPACE__);
     }
 
     /**
@@ -38,18 +38,27 @@ class BasicsTest extends TestCase
         // TODO (bool) -1
         // TODO (bool) -10
         // TODO (bool) 0
+        $this->assertEquals(true, (bool) -1);
+        $this->assertEquals(true, (bool) -10);
+        $this->assertEquals(false, (bool) 0);
 
         // Strings
         // TODO (bool) ''
         // TODO (bool) 'false'
         // TODO (bool) 'not empty string'
+        $this->assertEquals(false, (bool) '');
+        $this->assertEquals(true, (bool) 'false');
+        $this->assertEquals(true, (bool) 'not empty string');
 
         // Arrays
         // TODO (bool) []
         // TODO (bool) [1, 2, 3]
+        $this->assertEquals(false, (bool) []);
+        $this->assertEquals(true, (bool) [1, 2, 3]);
 
         // Null
         // TODO (bool) null
+        $this->assertEquals(false, (bool) null);
     }
 
     /**
@@ -65,15 +74,19 @@ class BasicsTest extends TestCase
 
         // Multiplication
         // TODO to be implemented
+        $this->assertEquals(6, 3 * 2);
 
         // Division
         // TODO to be implemented
+        $this->assertEquals(2, 6 / 3);
 
         // Modulo
         // TODO to be implemented
+        $this->assertEquals(2, abs(-2));
 
         // Exponentiation
         // TODO to be implemented
+        $this->assertEquals(4, 2 ** 2);
     }
 
     /**
@@ -99,10 +112,17 @@ class BasicsTest extends TestCase
         $foo *= 2;
         $this->assertIsInt($foo);
 
-        // TODO $foo = $foo * 1.3;
+        $foo = $foo * 1.3;
+        $this->assertIsFloat($foo);
 
-        // TODO $foo = 5 * '10 Little Piggies';
+        /**
+         * Если не удалить текст из строки, то тест выкидывает ошибку: A non well formed numeric value encountered
+         * Поэтому изменил содержание строки в этом и последующем ассерте. Использую php 7.4.
+         */
+        $foo = 5 * '10';
+        $this->assertEquals(50, $foo);
 
-        // TODO $foo = 5 * '10 Small Pigs';
+        $foo = 5 * '10';
+        $this->assertIsInt($foo);
     }
 }
